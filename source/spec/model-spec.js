fb1af1af7880e268de39496c0124b2dce8f33737
Jamie McKenzie <jamck23@gmail.com>
2014-05-22 13:44:34 -0700

    Adds model testing in jasmine.

--- source/spec/model-spec.js
+++ source/spec/model-spec.js
@@ -3,6 +3,23 @@ describe ("Grid", function(){
     var grid = new Grid();
     expect(grid).toBeDefined();
   });
+
+  it("should have a size of zero", function(){
+    var grid2 = new Grid();
+    expect(grid2.size).toEqual(0);
+  });
+
+  it("should have an empty array of tiles", function(){
+    var grid = new Grid();
+    expect(grid.tiles).toEqual([]);
+  })
+
+  it("should have a method addTile", function(){
+    var grid = new Grid();
+    var tile = new Tile();
+    grid.addTile(tile);
+    console.log(grid.tiles);
+  })
 })
 
 describe ("Tile", function(){
