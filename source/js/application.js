7e032fd83eee6e313c82ac40523870a360a4d519
Steven G. Harms <steven@devbootcamp.com>
2014-06-06 15:51:38 -0400

    Use local variables inside onReady IIFE
    These variables are being set on a global
    `ColorApp`.  Why?  No benefit.  Why not assign
    them to vars locally scoped within the app?  They
    won't bleed outside the IIFE.

--- source/js/application.js
+++ source/js/application.js
@@ -1,22 +1,23 @@
 ColorApp = {}; // This is the top-level file, define your namespace here
 
 $('document').ready(function(){
-  var selectors = {
+  var view, controller, binder,
+    selectors = {
     makeGridAction: "button.new",
     clearGridAction: "button.clear",
     saveGridAction: "submit",
   };
 
-  ColorApp.view = new ColorApp.View( {
+  view = new ColorApp.View( {
     gridSelector: ".default-grid ul",
     tileSelector: "li",
   });
 
-  ColorApp.controller = new ColorApp.Controller({view: ColorApp.view});
-  ColorApp.controller.loadDefaultGrid();
+  controller = new ColorApp.Controller({view: ColorApp.view});
+  controller.loadDefaultGrid();
 
-  ColorApp.binder = new ColorApp.Binder(selectors, ColorApp.controller);
-  ColorApp.binder.bind();
+  binder = new ColorApp.Binder(selectors, ColorApp.controller);
+  binder.bind();
 })
 
 
