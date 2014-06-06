e5dc030e4c3620e62f287022b2253f94a1fe58c8
Steven G. Harms <steven@devbootcamp.com>
2014-06-06 15:54:49 -0400

    Inline view definition

--- source/js/application.js
+++ source/js/application.js
@@ -1,19 +1,20 @@
 ColorApp = {}; // This is the top-level file, define your namespace here
 
 $('document').ready(function(){
-  var view, controller, binder,
+  var controller, binder,
     selectors = {
     makeGridAction: "button.new",
     clearGridAction: "button.clear",
     saveGridAction: "submit",
   };
 
-  view = new ColorApp.View({
-    gridSelector: ".default-grid ul",
-    tileSelector: "li",
+  controller = new ColorApp.Controller({
+    view: new ColorApp.View({
+      gridSelector: ".default-grid ul",
+      tileSelector: "li",
+    })
   });
 
-  controller = new ColorApp.Controller({view: ColorApp.view});
   controller.loadDefaultGrid();
 
   binder = new ColorApp.Binder(selectors, controller);
