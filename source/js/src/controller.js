f6af27754199d2d51c068d6889a078d82d3be8aa
Steven G. Harms <steven@devbootcamp.com>
2014-06-06 16:05:30 -0400

    Comments

--- source/js/src/controller.js
+++ source/js/src/controller.js
@@ -8,6 +8,16 @@ ColorApp.Controller.prototype = {
     this.grid = new Grid();
     this.populateGrid(this.grid);
     this.view.displayDefaultGrid(this.grid);
+
+    /* The above line is a classic give away of not understanding how OO should
+     * work.
+     *
+     * Why is the controller telling the View what to do to draw the screen?
+     * Why can't the controller throw itself to the view and then the *view*
+     * can decide what it needs from the thing it receives (i.e. .grid) to
+     * draw?  Here you're making the contorller know how to display.  Don't.
+     * Consult "Inversion of Dependency"
+     */
   },
 
   populateGrid: function(grid) {
@@ -18,6 +28,7 @@ ColorApp.Controller.prototype = {
 
   colorTile: function(e) {
     var index = e.target.getAttribute('class');
+    /* Gross! */
     var gridSelector = "." + e.target.parentElement.parentElement.getAttribute('class') + " ul";
     var tile = this.grid.tiles[index]
     var newColor = this.randColor();
@@ -26,6 +37,9 @@ ColorApp.Controller.prototype = {
   },
 
   randColor: function(){
+    /* See code smell called "MAGIC NUMBER" What do these
+     * numbers mean?
+     */
     return "#" + Math.floor(Math.random()*16777215).toString(16);
   },
 
@@ -40,13 +54,13 @@ ColorApp.Controller.prototype = {
   makeNewColoredGrid: function(e){
     var colors = this.getColors(e);
     var grid = new Grid(colors.length);
+    // Should probably be configurable
     grid.name = ($("#formValueID").val());
     this.populateGrid(grid);
     for (var i = 0; i < colors.length; i++) {
       grid.tiles[i].color = colors[i];
     }
     return grid;
-
   },
 
   saveGrid: function(e){
