e0a1132cdf8de394a69d21907cc3a9c4c8474aa1
Steven G. Harms <steven@devbootcamp.com>
2014-06-06 16:03:22 -0400

    Comments on Binder

--- source/js/src/binder.js
+++ source/js/src/binder.js
@@ -1,3 +1,46 @@
+/* I generally don't like a class like this.  I feel like
+ * the controller should have a method called init() or
+ * somethign like it that knows how to set up the events
+ * based on a config object (like you have in application.js)
+ *
+ * This Binder thing is a pattern I use with Phase 2's to break
+ * up the pattern of jQuery soup.  It's not terrible, but it's
+ * a bit unconventional
+ *
+ *
+ * It seems like there should be something like
+ *
+  controller = new ColorApp.Controller({
+    view: new ColorApp.View({
+      gridSelector: ".default-grid ul",
+      tileSelector: "li",
+    })
+  });
+
+  controller.loadDefaultGrid();
+
+  binder = new ColorApp.Binder(selectors, controller);
+
+
+ That moves to...
+
+  new ColorApp.Controller({
+    view: new ColorApp.View({
+      gridSelector: ".default-grid ul",
+      tileSelector: "li",
+    })
+  }).init();
+
+ WHERE init() does the work of...
+
+  this.loadDefaultGrid();
+
+ and
+
+  binder = new ColorApp.Binder(selectors, controller);
+ *
+ */
+
 ColorApp.Binder = function(targets, controller){
   this.targets = targets;
   this.controller = controller;
