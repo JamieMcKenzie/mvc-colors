d68cc74452a07a5e9c680893b14ce0413ddac2a0
Steven G. Harms <steven@devbootcamp.com>
2014-06-06 15:56:14 -0400

    Formatting

--- source/js/src/binder.js
+++ source/js/src/binder.js
@@ -5,37 +5,37 @@ ColorApp.Binder = function(targets, controller){
 
 ColorApp.Binder.prototype = {
   bind: function(){
-          this.bindClearGridAction();
-          this.bindColorTileAction();
-          this.bindSaveGridAction();
+    this.bindClearGridAction();
+    this.bindColorTileAction();
+    this.bindSaveGridAction();
   },
 
   bindClearGridAction: function(){
-                          var controller = this.controller,
-                          but = this.targets.clearGridAction;
+    var controller = this.controller,
+    but = this.targets.clearGridAction;
 
-                          $(but).on('click', function(){
-                            controller.loadDefaultGrid();
-                          })
+    $(but).on('click', function(){
+      controller.loadDefaultGrid();
+    })
   },
 
   bindColorTileAction: function(){
-                          var controller = this.controller,
-                          but = this.targets.tileSelector;
+    var controller = this.controller,
+    but = this.targets.tileSelector;
 
-                          $(".default-grid ul li").on('click', function(e){
-                            controller.colorTile(e);
-                          })
+    $(".default-grid ul li").on('click', function(e){
+      controller.colorTile(e);
+    })
   },
 
   bindSaveGridAction: function(){
-                          var controller = this.controller,
-                          but = this.targets.saveGridAction;
+    var controller = this.controller,
+    but = this.targets.saveGridAction;
 
-                          $("form").submit(function(e) {
-                            controller.saveGrid(e);
-                            e.preventDefault();
-                          })
+    $("form").submit(function(e) {
+      controller.saveGrid(e);
+      e.preventDefault();
+    })
   },
 
 
